b596471bfc6e09e6c7de965e8b9e64c09912b3a3
k0r0tk0ff <korotkov34reg@gmail.com>
2017-05-26 22:13:28 +0300

    1

---
+++ src/main/java/ru/k0r0tk0ff/main/Main.java
@@ -0,0 +1,11 @@
+package ru.k0r0tk0ff.main;
+
+/**
+ * Created by k0r0tk0ff on 5/26/2017.
+ */
+public class Main {
+    public static void main(String[] arg) throws ParserConfigurationException, IOException{
+
+
+    }
+}
